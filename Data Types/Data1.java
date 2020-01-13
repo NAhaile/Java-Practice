a0f88b58b32e5edd4ffc4793a7de2e3643469272
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-13 00:04:48 -0500

    Add files via upload

---
+++ Data Types/Data1.java
@@ -0,0 +1,36 @@
+public class Data1 {
+  public static void main(String args[]) {
+    //8 primitive data types
+    
+    boolean thing = false;  //<------- this has a size of 1 bit // true or false
+    byte thing2 = 0; //<------- this has a size of 8 bits and acts as an integer // range from -128 to 127
+    char thing3 = 'i'; //<------- this has a size of 16 bits // character representation of the ASCII values 0 to 255
+    short thing4 = 0; //<------- this has a size of 16 bits // and acts as an integer // range from -30,000 to 30,000
+    int thing5 = 0; //<------- this has a size of 32 bits // and is an integer // range in the millions
+    long thing6 = 0L; //<------- this has a size of 64 bits // and acts as an integer // range in the billions and trillions
+    float thing7 = 0.0f; //<------- this has a size of 32 bits // and is a float // range in the millions
+    double thing8 = 0.0d; //<------- this has a size of 64 bits // and acts as a float // range in the billions and trillions
+   
+    System.out.println(thing);
+    System.out.println(thing2);
+    System.out.println(thing3);
+    System.out.println(thing4);
+    System.out.println(thing5);
+    System.out.println(thing6);
+    System.out.println(thing7);
+    System.out.println(thing8);
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+}
