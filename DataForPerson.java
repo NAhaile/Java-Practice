04f96b3b6ebf6a01e8ba59c7d79cb27304328484
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-14 23:58:07 -0500

    Add files via upload

---
+++ DataForPerson.java
@@ -0,0 +1,16 @@
+public class DataForPerson {
+
+ public void main(String args[]) {
+  Person a = new Person();
+  a.name = "nate";
+  a.age = 20;
+
+
+
+  System.out.println("This is your name: " + a.name);
+
+
+ }
+
+
+}
