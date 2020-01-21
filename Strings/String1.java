fe5b76af8f6998c76b4056e0c64005871fd46abf
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-20 23:55:03 -0500

    Update String1.java

--- Strings/String1.java
+++ Strings/String1.java
@@ -1,7 +1,7 @@
 public class String1 {
  public static void main(String args[]) {
 
- String name = "Nate";
+ String name = "Nate"; //Note, String is a class thats why its Capital S and not string, like the other data types.
 
  System.out.println(name);
 
