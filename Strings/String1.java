7c6fd58f3d0fff7ff053b31342eb0140c0d81f65
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-20 23:55:19 -0500

    Update String1.java

--- Strings/String1.java
+++ Strings/String1.java
@@ -3,7 +3,7 @@ public class String1 {
 
  String name = "Nate"; //Note, String is a class thats why its Capital S and not string, like the other data types.
 
- System.out.println(name);
+ System.out.println(name); //This will print out the name variable onto the screen.
 
 }
 
